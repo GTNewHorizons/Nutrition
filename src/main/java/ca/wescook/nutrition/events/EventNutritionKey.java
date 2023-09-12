42cf5efca70dd78ac131e40962d2bfdd86e821eb
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-09-12 19:49:37 +0200

    rewrite keybind handler, there is no spectator mode in 1.7.10

--- src/main/java/ca/wescook/nutrition/events/EventNutritionKey.java
+++ src/main/java/ca/wescook/nutrition/events/EventNutritionKey.java
@@ -1,13 +1,8 @@
 package ca.wescook.nutrition.events;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.world.World;
 
-import org.lwjgl.input.Keyboard;
-
-import ca.wescook.nutrition.Nutrition;
-import ca.wescook.nutrition.gui.ModGuiHandler;
+import ca.wescook.nutrition.gui.NutritionGui;
 import ca.wescook.nutrition.proxy.ClientProxy;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
@@ -19,31 +14,10 @@ public class EventNutritionKey {
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public void keyInput(InputEvent.KeyInputEvent event) {
-        // Exit on key de-press
-        if (!Keyboard.getEventKeyState()) {
-            return;
-        }
-
-        // If Nutrition key is pressed, and F3 key is not being held (F3+N toggles Spectator mode)
-        if (ClientProxy.keyNutritionGui.getIsKeyPressed() && !Keyboard.isKeyDown(Keyboard.KEY_F3)) {
-            openNutritionGui();
+        if (ClientProxy.keyNutritionGui.isPressed()) {
+            Minecraft.getMinecraft()
+                .displayGuiScreen(new NutritionGui());
         }
     }
 
-    // Opens GUI to show nutrition menu
-    @SideOnly(Side.CLIENT)
-    private void openNutritionGui() {
-        // Get data
-        EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-        World world = Minecraft.getMinecraft().theWorld;
-
-        // Open GUI
-        player.openGui(
-            Nutrition.instance,
-            ModGuiHandler.NUTRITION_GUI_ID,
-            world,
-            (int) player.posX,
-            (int) player.posY,
-            (int) player.posZ);
-    }
 }
